406e677c545fc2d02531e849465a3142d53bf13e
cam39porter <cam.b.porter@gmail.com>
2018-03-03 20:27:05 -0500

    basic map with LSOAs

--- application/src/App.js
+++ application/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "tachyons";
 
-import { Map, Marker, Popup, GeoJSON } from "react-leaflet";
+import { Map, GeoJSON, TileLayer } from "react-leaflet";
 
-import lsoa from "./assets/lsoa.json";
+import lsoa from "./assets/Lower_Layer_Super_Output_Areas_December_2011_Super_Generalised_Clipped__Boundaries_in_England_and_Wales.json";
 
 const position = [51.505, -0.09];
 
@@ -11,15 +11,12 @@ class App extends Component {
   render() {
     return (
       <div className={`w-100 vh-100`}>
-        <Map center={position} zoom={13} className={`w-100 vh-100`}>
+        <Map center={position} zoom={6} className={`w-100 vh-100`}>
           <GeoJSON data={lsoa} />
-          <Marker position={position}>
-            <Popup>
-              <span>
-                A pretty CSS3 popup.<br />Easily customizable.
-              </span>
-            </Popup>
-          </Marker>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
         </Map>
       </div>
     );
